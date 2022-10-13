37ef15f99169692ba6157a80a7798102aa6927ab
FedericoMontero <fede17.m@gmail.com>
2022-10-12 22:34:31 -0300

    entrega finalisima v1

--- js/cartera.js
+++ js/cartera.js
@@ -2,9 +2,11 @@
     let disponible_dolares=2000
     let disponible_BTC=5
     let cotizacion_BTC=25
+    //muestro las variables en los elementos html
     document.getElementById("disponibledolares").innerHTML="USD "+disponible_dolares
     document.getElementById("disponiblebtc").innerHTML="BTC "+disponible_BTC
 
+    //esto no deberia entar aca peeero quedo jeje
 function comprar(){
     let disponible_dolares=2000
     let disponible_BTC=5
@@ -35,14 +37,18 @@ function vender(){
 //funcion basica de guardar datos en el localstorage
 guardar_datos();
 
+//guardo datos en el localstorage
 function guardar_datos(){
-    let usuario={
-        nombre:"Federico",
-        apellido:"Montero",
-        edad:22,
-        pais:"UY"
-    }
+    let usuario="Federico Montero"
+    let pais="Uruguay"
     localStorage.setItem("usuario",JSON.stringify(usuario));
+    localStorage.setItem("pais",JSON.stringify(pais));
 }
+
+//consumo esos datos en el local storage.
+var nombre = localStorage.getItem("usuario");
+var pais = localStorage.getItem("pais");     
+document.getElementById("profile_nombre").innerHTML = nombre;
+document.getElementById("profile_pais").innerHTML = pais; 
 
 
--- js/dataFetch.js
+++ js/dataFetch.js
@@ -1,7 +1,7 @@
 
 //Modulo Dashboard
 //API KEY: coinranking3b6b109547c3dcffe70adc9d9a4892ed0856809aa43432a6
-
+//aca sigo la documentacion de la API, considero que va con la tematica del proyecto.
 let baseUrl= "https://api.coinranking.com/v2/coins"
 let proxyUrl="https://cors-anywhere.herokuapp.com/"
 let apiKey="coinranking3b6b109547c3dcffe70adc9d9a4892ed0856809aa43432a6"
@@ -21,20 +21,20 @@ fetch(`${proxyUrl}${baseUrl}`,{
             let coinsData=json.data.coins
 
             if(coinsData.length>0){
-                let cryptoCoins=""
+                var cryptoCoins=""
             }
 
-            coinsData.forEach((coins)=>{
+            coinsData.forEach((coin)=>{
                 cryptoCoins+="<tr>"
                 cryptoCoins+=`<td>${coin.uuid}</td>`;
-                cryptoCoins+=`<td>${coin.uuid}</td>`;
-                cryptoCoins+=`<td>${coin.uuid}</td>`;
-                cryptoCoins+=`<td>${coin.uuid}</td>`;
-                cryptoCoins+=`<td>${coin.uuid}</td>`;
-                cryptoCoins+=`<td>${coin.uuid}</td>`;
-                cryptoCoins+=`<td>${coin.uuid}</td>`;
-            })//https://www.youtube.com/watch?v=-fn_cse80-4 
-            //min 25.04
+                cryptoCoins+=`<td>${coin.btcPrice}</td>`;
+                cryptoCoins+=`<td>${coin.rank}</td>`;
+                cryptoCoins+=`<td>${coin.tier}</td>`;
+                cryptoCoins+=`<td>${coin.name}</td>`;
+                cryptoCoins+=`<td>${coin.price}</td>`;
+                cryptoCoins+=`<td>${coin.symbol}</td>`;"<tr>";
+            })
+            document.getElementById("data").innerHTML=cryptoCoins
     })
 }
 }).catch((error)=>{
